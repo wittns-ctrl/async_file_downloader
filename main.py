24ad818c0e1dfb6b296403b207bf556c09cfc230
wittns <mugishawitness15@gmail.com>
2026-07-28 18:58:51 +0200

    multiple downloading using gather

--- main.py
+++ main.py
@@ -7,18 +7,11 @@ from downloader import Downloader
 async def main():
     downloader = Downloader()
 
-    task1 = asyncio.create_task(
-    
-    downloader.download("https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf","dummy.txt"))
+    await asyncio.gather(downloader.download("https://docs.google.com/document/d/1zEn9IzlGG2cwaLGG-L7kS2XX4brt6spRDw-gE_NL8AE/edit?tab=t.0","dammy.txt"),
 
-    await task1
-
-    task2 = asyncio.create_task(
-
-    downloader.download("https://docs.google.com/document/d/1zEn9IzlGG2cwaLGG-L7kS2XX4brt6spRDw-gE_NL8AE/edit?tab=t.0","dammy.txt"))
-
-    await task2
-
+                        downloader.download("https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf","dummy.txt"),
+                        downloader.download("https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf","dummy.txt"),
+                        downloader.download("https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf","dummy.txt"))
 
 if __name__ == "__main__":
     asyncio.run(main())
