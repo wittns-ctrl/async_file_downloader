392f54011d5f517e9a279f6812eb2a704f3cf702
wittns <mugishawitness15@gmail.com>
2026-07-28 18:52:23 +0200

    multiple file downloading using create_task

--- main.py
+++ main.py
@@ -6,7 +6,18 @@ from downloader import Downloader
 
 async def main():
     downloader = Downloader()
-    await downloader.download("https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf","dummy.txt")
+
+    task1 = asyncio.create_task(
+    
+    downloader.download("https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf","dummy.txt"))
+
+    await task1
+
+    task2 = asyncio.create_task(
+
+    downloader.download("https://docs.google.com/document/d/1zEn9IzlGG2cwaLGG-L7kS2XX4brt6spRDw-gE_NL8AE/edit?tab=t.0","dammy.txt"))
+
+    await task2
 
 
 if __name__ == "__main__":
