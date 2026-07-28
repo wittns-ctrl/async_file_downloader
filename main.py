581b50738b6bf2d083eb141ac90e948dcb95489d
wittns <mugishawitness15@gmail.com>
2026-07-28 19:50:08 +0200

    limiting downloads

--- main.py
+++ main.py
@@ -3,15 +3,22 @@
 import asyncio
 from downloader import Downloader
 
+semaphore = asyncio.Semaphore(2)
+
+
+async def limits(download,url,filename):
+        async with semaphore:
+             await download.download(url,filename)
+
 
 async def main():
-    downloader = Downloader()
+     
+        downloader = Downloader()
+        await asyncio.gather(limits(downloader,"https://docs.google.com/document/d/1zEn9IzlGG2cwaLGG-L7kS2XX4brt6spRDw-gE_NL8AE/edit?tab=t.0","dammy.txt"),
 
-    await asyncio.gather(downloader.download("https://docs.google.com/document/d/1zEn9IzlGG2cwaLGG-L7kS2XX4brt6spRDw-gE_NL8AE/edit?tab=t.0","dammy.txt"),
-
-                        downloader.download("https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf","dummy.txt"),
-                        downloader.download("https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf","dummy.txt"),
-                        downloader.download("https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf","dummy.txt"))
+                        limits(downloader,"https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf","dummy.txt"),
+                        limits(downloader,"https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf","dummy.txt"),
+                        limits(downloader,"https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf","dummy.txt"))
 
 if __name__ == "__main__":
     asyncio.run(main())
